487dc521f40e285399be770df4b182e9e78e0981
Ba <BA>
2025-12-07 10:57:58 +0700

    Update

--- arp-syncing/run.sh
+++ arp-syncing/run.sh
@@ -4,12 +4,12 @@ set -euo pipefail
 OPTIONS_FILE="/data/options.json" 
 
 if [ -f "$OPTIONS_FILE" ]; then
-  IP=$(jq -r '.ip // empty' "$OPTIONS_FILE")
-  USERNAME=$(jq -r '.username // empty' "$OPTIONS_FILE")
-  PASSWORD=$(jq -r '.password // empty' "$OPTIONS_FILE")
-  export IP
-  export USERNAME
-  export PASSWORD
+  MODEM_IP=$(jq -r '.ip // empty' "$OPTIONS_FILE")
+  MODEM_USERNAME=$(jq -r '.username // empty' "$OPTIONS_FILE")
+  MODEM_PASSWORD=$(jq -r '.password // empty' "$OPTIONS_FILE")
+  export MODEM_IP
+  export MODEM_USERNAME
+  export MODEM_PASSWORD
 fi
 
 exec bun run ./src/index.ts
